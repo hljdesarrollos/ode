a6362c3f2259fd9fea3b6b28099740df7c1be970
figueduff <figueduff@hotmail.com>
2023-03-13 18:05:08 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      Hola aca va la pagina de ode
+     en https://ode-odontologia.netlify.app se ve el previo o merge que se hace a rama desarrollo
     </div>
   )
 }
