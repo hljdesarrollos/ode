ad84afab8fbca848fa146146cfebfa3a91fe3d0e
figueduff <figueduff@hotmail.com>
2024-01-09 23:30:31 -0300

    link cards

--- src/components/home/CardTratamientos.jsx
+++ src/components/home/CardTratamientos.jsx
@@ -1,9 +1,11 @@
-const CardTratamientos = ({ img, text }) => {
+const CardTratamientos = ({ img, text, url}) => {
   return (
+    <a href={url}>
     <div className="t-card">
       <img src={img} alt="" className="t-card-image w-100" />
       <p className="t-card-text">{text}</p>
     </div>
+    </a>
   );
 };
 
